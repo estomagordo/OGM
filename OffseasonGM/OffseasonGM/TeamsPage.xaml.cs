132bb65510d2e3223e37936ba7687ffb7fe7fe86
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-03-31 09:25:59 +0200

    Removed commented out code.

--- OffseasonGM/OffseasonGM/TeamsPage.xaml.cs
+++ OffseasonGM/OffseasonGM/TeamsPage.xaml.cs
@@ -68,34 +68,4 @@ namespace OffseasonGM
             currentTeamIndex++;
         }
     }
-
-    //class TeamsPageViewModel //: INotifyPropertyChanged
-    //{
-
-    //    public TeamsPageViewModel()
-    //    {
-    //        //IncreaseCountCommand = new Command(IncreaseCount);
-    //    }
-
-    //    //int count;
-
-    //    //string countDisplay = "You clicked 0 times.";
-    //    //public string CountDisplay
-    //    //{
-    //    //    get { return countDisplay; }
-    //    //    set { countDisplay = value; OnPropertyChanged(); }
-    //    //}
-
-    //    public ICommand CycleBackCommand { get; }
-    //    public ICommand CycleForwardCommand { get; }
-
-    //    //void IncreaseCount() =>
-    //    //    CountDisplay = $"You clicked {++count} times";
-
-
-    //    //public event PropertyChangedEventHandler PropertyChanged;
-    //    //void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
-    //    //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-    //}
 }
