026321a5c82321ee2aa7e68a39d9aa1c63bec4a4
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-04-03 08:07:46 +0200

    Removed some default comments.

--- OffseasonGM/OffseasonGM/App.xaml.cs
+++ OffseasonGM/OffseasonGM/App.xaml.cs
@@ -63,17 +63,17 @@ namespace OffseasonGM
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            
         }
 
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
+            
         }
 
         protected override void OnResume()
         {
-            // Handle when your app resumes
+            
         }
 
         private void InitializeRepositories(string dbPath)
