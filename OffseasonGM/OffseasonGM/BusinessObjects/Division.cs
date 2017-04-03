a4c5f8af769b7eaff0b237271c9ae7748f5fe7d6
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-04-03 11:07:16 +0200

    First BusinessObject: Division.

---
+++ OffseasonGM/OffseasonGM/BusinessObjects/Division.cs
@@ -0,0 +1,28 @@
+﻿using OffseasonGM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffseasonGM.BusinessObjects
+{
+    public class Division
+    {
+        public List<Team> Teams { get; set; }
+        public double AverageLatitude
+        {
+            get
+            {
+                return Teams.Average(team => team.City.Latitude);
+            }
+        }
+        public double AverageLongitude
+        {
+            get
+            {
+                return Teams.Average(team => team.City.Longitude);
+            }
+        }
+    }
+}
