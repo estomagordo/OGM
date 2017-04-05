c6d58e2139a2633edf63b8c2034c801e7cc7f6ea
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-04-05 11:09:13 +0200

    Start of a test. Doesn't quite work yet, in that the content of the mocked locator does not get mocked.

--- Tests/BusinessTests.cs
+++ Tests/BusinessTests.cs
@@ -1,9 +1,24 @@
 ﻿using NUnit.Framework;
+using Moq;
+using OffseasonGM.Assets.Managers;
+using OffseasonGM.Models;
 
 namespace Tests
 {
     [TestFixture]
     public class BusinessTests
     {
+        [Test]
+        public void AllTeamsPlayCorrectAmountOfHomeAndAwayGames()
+        {
+            var mockLocator = new Mock<IRepositoryLocator>();            
+            var manager = new GameManager(mockLocator.Object, "");
+
+            manager.CreateLeague(League.LeagueConfiguration.Teams30Divisions4, 2016);
+            manager.SetupLeague();
+            manager.PlaySeason();
+
+            Assert.That(manager.Teams.TrueForAll(team => team.AwayGames.Count == 41 && team.HomeGames.Count == 41));
+        } 
     }
 }
